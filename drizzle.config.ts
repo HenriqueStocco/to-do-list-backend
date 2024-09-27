db5faba48c08b16ad91788c1b6d485cad394a6bd
Henrique Stocco <henriquestoccomachado@gmail.com>
2024-09-27 11:17:06 -0300

    fix: env schema import updated

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig, type Config } from 'drizzle-kit'
-import { env } from './src/env'
+import { env } from './src/lib/env'
 
 export default defineConfig({
   schema: './src/db/schema.ts',
